1789196348726f2fbc428d3c288018d60024009a
Matti Nelimarkka <matti.nelimarkka@gmail.com>
2012-08-01 15:02:18 +0300

    Separate groups and display group spesific content for each group

--- server/chat.js
+++ server/chat.js
@@ -7,6 +7,15 @@ var nowjs = require("now");
 
 var everyone = nowjs.initialize(httpServer);
 
+var groups = []
+
+// get deciderd number of groups
+for( var i = 0; i < 2; i++ ) {
+    var group = nowjs.getGroup("group-" + i);
+    groups.push( group );
+}
+
+// common content
 everyone.msgId = 0;
 everyone.log = [];
 everyone.votes = [];
@@ -19,6 +28,8 @@ everyone.now.vote = function(message, voter) {
 }
 
 everyone.now.distributeMessage = function(message, response ){
+  var variant = this.now.user.variant;
+
   var msgId = ++ everyone.msgId;
   // clear response's response
   response.response = null;
@@ -32,11 +43,17 @@ everyone.now.distributeMessage = function(message, response ){
     response : response,
     depth: response.depth + 1,
   }
+
+  // TODO: vary the messages based on variant
+  if( variant == 0 ) {
+      console.log( 'Should be anonym' );
+      msg.anonym = true;
+  }
+  console.log( variant );
   console.log( msg );
   everyone.log.push( msg );
   console.log( everyone.log );
-  everyone.now.receiveMessage( msg );
-
+  groups[ variant ].now.receiveMessage( msg );
   if( message == '/clearall' ) {
        everyone.log = []
        everyone.votes = []
@@ -48,7 +65,9 @@ everyone.userId = 0;
 
 nowjs.on( 'connect' , function() {
    var id = ++ everyone.userId;
-   this.now.user = { id : id, variant : id % 4 };
+   var group = id % groups.length;
+   this.now.user = { id : id, variant : group };
+   groups[ group ].addUser( this.user.clientId );
    // push log
    for( var e in everyone.log ) {
       e = everyone.log[ e ];
